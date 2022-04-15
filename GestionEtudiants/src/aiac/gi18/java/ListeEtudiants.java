b9d10abaa1775d8dd6caa01b8b453beebcb1a072
Ordinateur <Ordinateur@DESKTOP-063H2KH>
2022-04-15 11:57:58 +0000

    methode add

--- GestionEtudiants/src/aiac/gi18/java/ListeEtudiants.java
+++ GestionEtudiants/src/aiac/gi18/java/ListeEtudiants.java
@@ -3,6 +3,14 @@ package aiac.gi18.java;
 import java.util.ArrayList;
 public class ListeEtudiants extends ArrayList<Etudiant>
 {
+	@Override
+	public boolean add(Etudiant e)
+	{
+		if(this.contains(e)) 
+			return false;
+		return super.add(e) ;
+		
+	}
 	
 	
 
