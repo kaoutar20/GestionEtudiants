46bde962b5ee4ea5dfdcfa8244993fde2f4b5951
Ordinateur <Ordinateur@DESKTOP-063H2KH>
2022-04-15 10:20:53 +0000

    second

--- GestionEtudiants/src/aiac/gi18/java/Etudiant.java
+++ GestionEtudiants/src/aiac/gi18/java/Etudiant.java
@@ -1,5 +1,15 @@
 package aiac.gi18.java;
 
-public class Etudiant {
+public class Etudiant 
+{
+	private int id;
+	private String nom;
+	private double  note;
+	
+	public Etudiant(String nom, double note)
+	{
+		note=10;
+	}
+	
 
 }
